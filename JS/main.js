fee8c4b54c0dffc85e6a8eadca15ab4f17d4534f
xomyak-coder <dkhol@inbox.ru>
2021-12-17 00:36:41 +0300

    Commit

---
+++ JS/main.js
@@ -0,0 +1,22 @@
+// $(document).ready(function () {
+//   const mainlSwiper = new Swiper('.slider-main', {
+//   // Optional parameters
+//   loop: true,
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.slider-main__button--next',
+//     prevEl: '.slider-main__button--prev',
+//   },
+
+//   effect: "coverFlow",
+
+//   keyboard: {
+//     enabled: true,
+//     pageUpDown: true
+//   }, 
+
+//   allowTouchMove: 'true'
+
+// });
+// });
