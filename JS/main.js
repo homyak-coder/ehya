d0559791a93c6a8eeba3176bbb6cfc187c07cf02
xomyak-coder <dkhol@inbox.ru>
2021-12-20 00:51:56 +0300

    do: diplom project

--- JS/main.js
+++ JS/main.js
@@ -1,22 +1,98 @@
-// $(document).ready(function () {
-//   const mainlSwiper = new Swiper('.slider-main', {
-//   // Optional parameters
-//   loop: true,
+$(document).ready(function () {
+  const mainlSwiper = new Swiper('.swiper', {
+  slidesPerView: 4,
+  spaceBetween: 27,
+  // Optional parameters
+  loop: false,
 
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: '.slider-main__button--next',
-//     prevEl: '.slider-main__button--prev',
-//   },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-main__button--next',
+    prevEl: '.swiper-main__button--prev',
+    disabledClass: 'swiper-button-off',
+  },
 
-//   effect: "coverFlow",
+  effect: "coverFlow",
 
-//   keyboard: {
-//     enabled: true,
-//     pageUpDown: true
-//   }, 
+  keyboard: {
+    enabled: true,
+    pageUpDown: true
+  }, 
 
-//   allowTouchMove: 'true'
+  allowTouchMove: 'true'
 
-// });
-// });
+});
+
+$(document).ready(function () {
+  const mainlSwiper = new Swiper('.unrealised-slider', {
+  slidesPerView: 5,
+  spaceBetween: 30,
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.unrealised-slider__button--next',
+    prevEl: '.unrealised-slider__button--prev',
+  },
+
+  effect: "coverFlow",
+
+  keyboard: {
+    enabled: true,
+    pageUpDown: true
+  }, 
+
+  allowTouchMove: 'true'
+
+});
+});
+$('.story-videos-main__button--main').on('click', function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player-zero', {
+      height: '380',
+      width: '100%',
+      videoId: 'F2KZUrL9vZE',
+       events: {
+        'onReady': playVideo,
+       }
+    });
+  })
+
+$('.story-videos-buttom__one').on('click', function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      height: '200',
+      width: '100%',
+      videoId: 'ePe3nFtYB2U',
+       events: {
+        'onReady': playVideo,
+       }
+    });
+  })
+
+$('.story-videos-buttom__two').on('click', function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player-two', {
+      height: '200',
+      width: '100%',
+      videoId: '7G-rSsF2qwc',
+       events: {
+        'onReady': playVideo,
+       }
+    });
+  })
+
+$('.story-videos-buttom__three').on('click', function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player-three', {
+      height: '200',
+      width: '100%',
+      videoId: 'yKnIgCtQqNs',
+       events: {
+        'onReady': playVideo,
+       }
+    });
+  })
+  
+  function playVideo(event) {
+    event.target.playVideo();
+  }
+
+});
